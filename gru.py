8883078ac96c72bbbdc0c55212f49d8184a3b93a
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2019-04-14 22:58:37 -0400

    finish GRU model

--- gru.py
+++ gru.py
@@ -27,7 +27,7 @@ class GRU(nn.Module):
         self.linear = nn.Linear(in_features=self.gru_hidden_size, out_features=2)
     
     def initHidden(self, batch_size):
-        return torch.zeros(self.gru_layers, batch_size, self.gru_hidden_size)
+        return torch.zeros(self.gru_layers, batch_size, self.gru_hidden_size).cuda()
     
     def forward(self, input, hidden=None):
         if not hidden:
@@ -51,19 +51,37 @@ def train_model(model, num_epochs, train_loader, test_loader, lr):
             optimizer.step()
             if i % 10 == 0:
                 print('[%5d, %5d] loss: %.3f' % (epoch + 1, i + 1, loss.item()))
-    # TBD ...
+    if test_loader:
+        total = 0
+        correct = 0
+        for data in test_loader:
+            inputs, labels = data
+            outputs, _ = model(inputs)
+            _, indices = torch.max(outputs, 1)
+            total += indices.size()[0]
+            correct += torch.sum(indices == labels).tolist()
+        print((total, correct, correct/total))
 
 
 if __name__ == "__main__":
-    model = GRU()
+    model = GRU().cuda()
     f = open("data/vocab.pickle", 'rb')
     vocab = pickle.load(f)
     f.close()
+    #
     m_pos = sentences2matrix("data/train_pos_merged.txt", vocab)
     m_neg = sentences2matrix("data/train_neg_merged.txt", vocab)
     m = np.vstack((m_pos, m_neg))
     labels = np.array([1]*1500 + [0]*1500, dtype=int)
-    train_data = TensorDataset(torch.from_numpy(m), torch.from_numpy(labels))
-    train_loader = DataLoader(train_data, shuffle=True, batch_size=40)
-    train_model(model, 5, train_loader, None, 0.0001)
+    train_data = TensorDataset(torch.from_numpy(m).cuda(), torch.from_numpy(labels).cuda())
+    train_loader = DataLoader(train_data, shuffle=True, batch_size=100)
+    #
+    m_pos = sentences2matrix("data/test_pos_merged.txt", vocab)
+    m_neg = sentences2matrix("data/test_neg_merged.txt", vocab)
+    m_test = np.vstack((m_pos, m_neg))
+    labels_test = np.array([1]*1500 + [0]*1500, dtype=int)
+    test_data = TensorDataset(torch.from_numpy(m_test).cuda(), torch.from_numpy(labels_test).cuda())
+    test_loader = DataLoader(test_data, shuffle=True, batch_size=100)
+    #
+    train_model(model, 80, train_loader, test_loader, 0.0001)
 
